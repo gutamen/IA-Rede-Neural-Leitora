1ad34bb7272f82c5ea484b5d52c2f54267cd2647
Gustavo A Martini <gutamen@live.com>
2023-07-31 17:52:21 -0300

    novos

--- IA/src/main/java/com/mycompany/ia/IA.java
+++ IA/src/main/java/com/mycompany/ia/IA.java
@@ -4,6 +4,12 @@
 
 package com.mycompany.ia;
 
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.util.List;
+import java.util.Properties;
 import org.tensorflow.Graph;
 import org.tensorflow.Session;
 import org.tensorflow.Tensor;
@@ -18,54 +24,97 @@ import org.tensorflow.TensorFlow;
 
 public class IA {
 
-
-     private static final int CATEGORIES = 5;
-    private static final int EPOCHS = 10;
-    private static final int BATCH_SIZE = 32;
+private static final List<String> categories = List.of("wheather", "music", "politic", "economy", "tecnology");
 
     public static void main(String[] args) {
-
-        // Load the data
-        String[] documents = {
-            "O tempo está quente hoje",
-            "A nova música do Justin Bieber é ótima",
-            "O presidente foi eleito",
-            "A economia está em recessão",
-            "A nova tecnologia é revolucionária"
-        };
-
-        // Create the model
-        TensorFlow tf =  new TensorFlow();
         
         
-        Graph graph = new Graph();
-        createSession(graph);
-        
-        // Create the input tensor
-        Tensor inputTensor = tf.constant(documents);
 
-        // Create the output tensor
-        Tensor outputTensor = tf.matmul(inputTensor, tf.constant(new int[]{128, CATEGORIES}));
+        // Documento de texto de entrada para classificação
+        String inputText = "Insira aqui o texto da notícia que você deseja classificar.";
+        int numCategories = 5;
+        // Carregar o modelo pré-treinado (certifique-se de que o caminho esteja correto)
+        String modelPath = "path/to/your/model";
+        try (Graph graph = new Graph()) {
+            byte[] modelBytes = TensorFlow.loadLibrary(modelPath);
+            graph.importGraphDef(modelBytes);
 
-        // Compile the model
-        tf.compile(outputTensor, "softmax");
+            // Realizar pré-processamento do texto (por exemplo, tokenização e padding)
+            float[] inputTensorData = preprocessText(inputText);
 
-        // Train the model
-        tf.fit(inputTensor, outputTensor, EPOCHS, BATCH_SIZE);
+            // Crie uma sessão e execute o modelo para fazer a classificação
+            try (Session session = new Session(graph);
+                 Tensor<Float> inputTensor = Tensor.create(new long[]{1, inputTensorData.length}, FloatBuffer.wrap(inputTensorData));
+                 Tensor outputTensor = session.runner()
+                         .feed("input_tensor_name", inputTensor)
+                         .fetch("output_tensor_name")
+                         .run()
+                         .get(0)) {
 
-        // Evaluate the model
-        int accuracy = tf.evaluate(inputTensor, outputTensor)[1];
-        System.out.println("Accuracy: " + accuracy);
+                // Obtenha o resultado da classificação
+                float[] predictions = outputTensor.copyTo(new float[1][numCategories])[0];
+//                int predictedCategoryIndex = argmax(predictions);
+//                String predictedCategory = getCategoryName(predictedCategoryIndex);
 
-        // Classify a new document
-        String newDocument = "O novo álbum do Taylor Swift é incrível";
-        int category = tf.predict(newDocument)[0];
-        System.out.println("Category: " + category);
+                // Imprima a categoria prevista
+//                System.out.println("Categoria prevista: " + predictedCategory);
+            }
+        }    
     }
-    
-    public static Session createSession(Graph graph) {
-        Session session = new Session(graph);
-        return session;
+
+    private static float[] preprocessText(String text) {
+        // Configurar o pipeline de NLP do CoreNLP
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // Processar o texto com o pipeline de NLP
+        CoreDocument document = new CoreDocument(text);
+        pipeline.annotate(document);
+
+        // Obter a lista de tokens do texto
+        List<CoreLabel> tokens = document.tokens();
+
+        // Realizar outras etapas de pré-processamento, como remoção de stopwords e padding
+        // Aqui, vamos apenas converter os tokens em um vetor numérico
+        float[] inputTensorData = new float[tokens.size()];
+        for (int i = 0; i < tokens.size(); i++) {
+            // Represente cada token com um valor numérico simples (índice do token na lista de categorias)
+            String token = tokens.get(i).originalText().toLowerCase();
+            int tokenIndex = categories.indexOf(token);
+            if (tokenIndex >= 0) {
+                inputTensorData[i] = tokenIndex;
+            } else {
+                // Se o token não estiver em nenhuma categoria, atribua um valor padrão (por exemplo, -1)
+                inputTensorData[i] = -1;
+            }
+        }
+
+        return inputTensorData;
+    }
+//
+//    private static int argmax(float[] array) {
+//        // Encontre o índice do maior valor no array
+//    }
+//
+    private static String getCategoryName(int categoryIndex) {
+        // Mapeie o índice da categoria para o nome da categoria (clima, música, política, economia, tecnologia)
+        String returnS = null;
+        
+        switch(categoryIndex){
+            case(0):
+                returnS = "weather";
+            case(1):
+                returnS = "music";
+            case(2):
+                returnS = "politic";
+            case(3):
+                returnS = "economy";
+            case(4):
+                returnS = "tecnology";
+        }
+        
+        return returnS;
     }
     
    
